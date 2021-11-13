0f3359d351edd344941c13e8b421c19d7b0d38ff
Bob Wilmes <bobwilmes@gmail.com>
2021-11-12 22:25:20 -0600

    Add forms

---
+++ aers/android/app/src/main/kotlin/com/example/aers/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ aers/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'dog_model.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    /// MaterialApp is the base Widget for your Flutter Application
+    /// Gives us access to routing, context, and meta info functionality.
+    return MaterialApp(
+      title: 'We Rate Dogs',
+      // Make all our text default to white
+      // and backgrounds default to dark
+      theme: ThemeData(brightness: Brightness.dark),
+      home: MyHomePage(title: 'We Rate Dogs'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    /// Scaffold is the base for a page.
+    /// It gives an AppBar for the top,
+    /// Space for the main body, bottom navigation, and more.
+    return Scaffold(
+      /// App bar has a ton of functionality, but for now lets
+      /// just give it a color and a title.
+      appBar: AppBar(
+        /// Access this widgets properties with 'widget'
+        title: Text(widget.title),
+        backgroundColor: Colors.black87,
+      ),
+
+      /// Container is a convenience widget that lets us style it's
+      /// children. It doesn't take up any space itself, so it
+      /// can be used as a placeholder in your code.
+      body: Container(),
+    );
+  }
+}
